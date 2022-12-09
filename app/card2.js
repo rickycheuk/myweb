e44170b190a3dc053ebc415d807e38535aa1d40b
Ricky <rickycheuk524@gmail.com>
2022-12-09 17:04:34 -0500

    update layout

--- app/card2.js
+++ app/card2.js
@@ -6,11 +6,11 @@ import Tilt from './tilt'
 
 const Card = () => {
   const colors = [
-    { value: '#b6a179' },
-    { value: '#272425' },
-    { value: '#6389cb' },
-    { value: '#f2c758' },
     { value: '#ffffff' },
+    { value: '#f2c758' },
+    { value: '#b6a179' },
+    { value: '#6389cb' },
+    { value: '#272425' },
     { value: '#000000' },
   ];
 
@@ -19,22 +19,22 @@ const Card = () => {
     <div className='object-scale-down'>
         {/* card */}
         <Tilt options={{ glare: true , "max-glare":0.3}}  className="rounded-[30px] object-scale-down">
-            <div className="abolute min-w-[70px] min-h-[115px] max-w-[335px] max-h-[580px] bg-stone-900 rounded-[30px] border-[2px] border-stone-700 px-[30px] py-[30px] cursor-grab" >
+            <div className="abolute min-w-[70px] min-h-[115px] max-w-[275px] max-h-[460px] bg-stone-900 rounded-[22px] border-[2px] border-stone-700 px-[22px] py-[22px] cursor-grab" >
                 {/* card image */} 
-                <img className="w-full rounded-xl overflow-hidden mb-6" src="./ricky.jpg" alt='' draggable='false' />
+                <img className="w-full rounded-xl overflow-hidden mb-3" src="./ricky.jpg" alt='' draggable='false' />
                 {/* card title */}
-                <h1 className='text-3xl text-white mb-3 font-bold text-center'>Ricky Cheuk</h1>
+                <h1 className='text-2xl text-white mb-2 font-bold text-center'>Ricky Cheuk</h1>
                 {/* card subtitle */}
-                <p className='text-white mb-6 text-center'>- Software Engineer -</p>
+                <p className='text-white mb-3 text-center'>- Software Engineer -</p>
                 {/* button wrapper */}
-                <div className='flex justify-center items-center gap-x-[20px] mb-6'> 
-                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[20px] px-[20px] rounded-lg'>
+                <div className='flex justify-center items-center gap-x-[15px] mb-3'> 
+                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[10px] px-[10px] rounded-lg'>
                     Link1
                 </button>
-                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[20px] px-[20px] rounded-lg'>
+                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[10px] px-[10px] rounded-lg'>
                     Link2
                 </button>
-                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[20px] px-[20px] rounded-lg'>
+                <button className='bg-[#2d2b2c] text-white text-base font-medium py-[10px] px-[10px] rounded-lg'>
                     Link3
                 </button>
                 </div>
